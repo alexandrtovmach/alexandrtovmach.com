59aa4cb5804198eb2b2a4d367d6c00785f40621b
alexandrtovmach <alexandrtovmach@gmail.com>
2018-07-09 22:43:57 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.scss';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
