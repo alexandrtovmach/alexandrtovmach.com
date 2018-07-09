8bb20b2aca8dc4f2a3e6a29676d844c76eca7be6
alexandrtovmach <alexandrtovmach@gmail.com>
2018-07-09 23:13:24 +0300

    minimal component structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
+import classnames from "classnames";
 
-class App extends React.Component {
+import Header from "./Header";
+
+export default class App extends React.Component {
   render() {
     return (
-      <div className="">
+      <div className={classnames("app-container", {
+        "dark-theme": true, 
+        "light-theme": false, 
+      })}>
+        <Header />
         We are started development
       </div>
     );
   }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.scss';
 import App from './components/App';
+import './styles/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
