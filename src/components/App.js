a151e84f9e9897bed337b735ed7f5cb525dc5e6d
alexandrtovmach <alexandrtovmach@gmail.com>
2018-07-10 00:25:09 +0300

    theme manager implementing

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,24 @@ import classnames from "classnames";
 import Header from "./Header";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { themeChecked: false };
+    this.handleThemeChange = this.handleThemeChange.bind(this);
+  }
+ 
+  handleThemeChange(themeChecked) {
+    this.setState({ themeChecked });
+  }
+
   render() {
     return (
       <div className={classnames("app-container", {
-        "dark-theme": true, 
-        "light-theme": false, 
+        "dark-theme": this.state.themeChecked, 
+        "light-theme": !this.state.themeChecked, 
       })}>
-        <Header />
+        <Header handleThemeChange={this.handleThemeChange} themeChecked={this.state.themeChecked} />
+        <div className="hidden-header"></div>
         We are started development
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
 import React from "react";
+import Switch from "react-switch";
 
 export default class Header extends React.Component {
   render() {
     return (
-      <div className={"header"}>
+      <div className="header">
         Header
+        <label htmlFor="normal-switch">
+          <span>Switch with default style</span>
+          <Switch
+            onChange={this.props.handleThemeChange}
+            checked={this.props.themeChecked}
+            id="normal-switch"
+            onColor="#FFB300"
+            offColor="#280671"
+            onHandleColor="#eee"
+            offHandleColor="#eee"
+            checkedIcon={<span>M</span>}
+            uncheckedIcon={<span>S</span>}
+          />
+        </label>
       </div>
     );
   }
