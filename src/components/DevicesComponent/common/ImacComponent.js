c49f34b98aef7c7c3509b36d24b594dbc54ecd90
alexandrtovmach <alexandrtovmach@gmail.com>
2018-08-24 15:12:00 +0300

    fixed SVG iPad issue

--- src/components/DevicesComponent/common/ImacComponent.js
+++ src/components/DevicesComponent/common/ImacComponent.js
@@ -1,13 +1,13 @@
 import React from "react";
 import classnames from "classnames";
 
-import { ReactComponent as IMacSVG } from "../../../styles/assets/images/IMac.svg";
+import IMacSVG from "../../../styles/assets/images/IMac.svg";
 
 export default props => {
   return (
     <div className={classnames("device", "desktop-device", "no-theme")}>
       <img src={props.img} alt="desktop-screenshot" />
-      <IMacSVG />
+      <img src={IMacSVG} />
     </div>
   );
 };
--- src/components/DevicesComponent/common/IphoneComponent.js
+++ src/components/DevicesComponent/common/IphoneComponent.js
@@ -1,13 +1,13 @@
 import React from "react";
 import classnames from "classnames";
 
-import { ReactComponent as IPhoneSVG } from "../../../styles/assets/images/IPhone_X.svg";
+import IPhoneSVG from "../../../styles/assets/images/IPhone_X.svg";
 
 export default props => {
   return (
     <div className={classnames("device", "phone-device", "no-theme")}>
       <img src={props.img} alt="phone-screenshot" />
-      <IPhoneSVG />
+      <img src={IPhoneSVG} />
     </div>
   );
 };
