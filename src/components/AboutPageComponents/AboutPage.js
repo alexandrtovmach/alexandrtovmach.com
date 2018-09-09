38c031727fff7439682a85e6b8c082c15e1dcb41
alexandrtovmach <alexandrtovmach@gmail.com>
2018-09-09 22:31:10 +0300

    added low poly me

--- src/components/AboutPageComponents/AboutPage.js
+++ src/components/AboutPageComponents/AboutPage.js
@@ -1,8 +1,18 @@
 import React from "react";
 import classnames from "classnames";
 
+import AlexSVGSrc from "../../styles/assets/images/alex.svg";
+
 export default class About extends React.Component {
   render() {
-    return <div className={classnames("")}>{this.props.langPack.about}</div>;
+    return (
+      <div className={classnames("about-page")}>
+        <img
+          src={AlexSVGSrc}
+          alt="it's me"
+          className={classnames("alex-svg", {})}
+        />
+      </div>
+    );
   }
 }
