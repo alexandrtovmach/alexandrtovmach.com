45818a5a6339b7d0ac3a1db0ff802937fd0d03ad
alexandrtovmach <alexandrtovmach@gmail.com>
2018-09-30 16:30:14 +0300

    started working on About page

--- src/components/AboutPageComponents/AboutBackground.js
+++ src/components/AboutPageComponents/AboutBackground.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import AlexSVGSrc from "../../styles/assets/images/alex.svg";
+
 export default props => {
   const generateStars = (count = 100) => {
     const arr = [];
@@ -16,6 +18,11 @@ export default props => {
   return (
     <div className="background-about">
       <div className="stars">{generateStars(20)}</div>
+      <img src={AlexSVGSrc} alt="it's me" className="alex-img" />
+      <div className="alex-title-container">
+        <span>How create image like this?</span>
+        <a href="https://youtube.com">Watch on my YouTube</a>
+      </div>
     </div>
   );
 };
--- src/components/AboutPageComponents/AboutPage.js
+++ src/components/AboutPageComponents/AboutPage.js
@@ -1,18 +1,8 @@
 import React from "react";
 import classnames from "classnames";
 
-import AlexSVGSrc from "../../styles/assets/images/alex.svg";
-
 export default class About extends React.Component {
   render() {
-    return (
-      <div className={classnames("about-page")}>
-        <img
-          src={AlexSVGSrc}
-          alt="it's me"
-          className={classnames("alex-svg", {})}
-        />
-      </div>
-    );
+    return <div className={classnames("about-page")} />;
   }
 }
