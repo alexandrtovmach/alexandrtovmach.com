3995e6a89463f3ed3ee3bec1f985788d64e93ec5
alexandrtovmach <alexandrtovmach@gmail.com>
2019-03-27 13:22:14 +0200

    added webp qr

--- utils/task.js
+++ utils/task.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const fs = require("fs");
+const webp = require('webp-converter');
 const { generateResponsiveImages } = require("responsive-images-generator/lib/index");
+
+const IMAGES_DIR = path.join(`${__dirname}/images`);
 
 const configs = [
   {width: '25%', rename: {suffix: '@1x'}},
@@ -8,6 +11,16 @@ const configs = [
   {width: '100%', rename: {suffix: '@3x'}}
 ];
 
-const images = fs.readdirSync(path.join(__dirname, 'images')).map(file => path.join(__dirname, `images/${file}`))
-
-generateResponsiveImages(images, configs);
+Promise.all(
+  fs.readdirSync(IMAGES_DIR).map(file => {
+    return new Promise(resolve => {
+      const [name, ext] = file.split(".");
+      if (ext !== "webp") {
+        webp.cwebp(`${IMAGES_DIR}/${file}`, `${IMAGES_DIR}/${name}.webp`, "-q 50", () => resolve(`${IMAGES_DIR}/${name}.webp`));
+      } else {
+        resolve(`${IMAGES_DIR}/${file}`);
+      }
+    })
+  })
+)
+.then(images => generateResponsiveImages(images, configs));
