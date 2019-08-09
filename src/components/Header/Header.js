cd17913fc93903413539f4dbd17ba76cd204eaa6
alexandrtovmach <alexandrtovmach@gmail.com>
2019-08-09 21:15:55 +0300

    fix: Added aria-checked for accessibility

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,38 +10,38 @@ import "./Header.scss";
 
 export default class Header extends React.Component {
   render() {
+    const { langPack, handleLanguageChange, theme, handleThemeChange } = this.props;
+    const isLightMode = theme.name === "light-theme";
     return (
       <div className="header">
-        <a className="logo theme-fill-second" href="/" title={this.props.langPack.go_to_main_page}>
+        <a className="logo theme-fill-second" href="/" title={langPack.go_to_main_page}>
           <LogoSVG />
         </a>
         <div className="control-panel">
           <div className="language-switcher">
             <LangSwitcher
-              handleLanguageChange={this.props.handleLanguageChange}
-              languageWord={this.props.langPack.choose_language}
-              langPack={this.props.langPack}
+              handleLanguageChange={handleLanguageChange}
+              languageWord={langPack.choose_language}
+              langPack={langPack}
             />
           </div>
           <div
             style={{ fontSize: "initial" }}
-            title={
-              this.props.theme.name === "light-theme"
-                ? this.props.langPack.switch_to_night_mode
-                : this.props.langPack.switch_to_day_mode
-            }
+            title={isLightMode ? langPack.switch_to_night_mode : langPack.switch_to_day_mode}
           >
             <Switch
               className="theme-switcher theme-color-main theme-fill-main"
               width={60}
               height={30}
+              role="switch"
+              aria-checked={isLightMode}
               handleDiameter={24}
-              onChange={this.props.handleThemeChange}
-              checked={this.props.theme.name === "light-theme"}
-              onColor={this.props.theme.secondColor}
-              offColor={this.props.theme.secondColor}
-              onHandleColor={this.props.theme.mainColor}
-              offHandleColor={this.props.theme.mainColor}
+              onChange={handleThemeChange}
+              checked={isLightMode}
+              onColor={theme.secondColor}
+              offColor={theme.secondColor}
+              onHandleColor={theme.mainColor}
+              offHandleColor={theme.mainColor}
               checkedIcon={<MoonSVG />}
               uncheckedIcon={<SunSVG />}
             />
