21fbad4c664631e077b54e467a2e0078aad3863c
alexandrtovmach <alexandrtovmach@gmail.com>
2019-10-26 20:59:39 +0300

    fix: Fixed swipe navigation; Refactored language

--- src/components/CalendarPage/CalendarPage.js
+++ src/components/CalendarPage/CalendarPage.js
@@ -3,6 +3,6 @@ import classnames from "classnames";
 
 export default class Calendar extends React.Component {
   render() {
-    return <div className={classnames("")}>{this.props.langPack.calendar}</div>;
+    return <div className={classnames("")}>{this.props.langPack.schedule}</div>;
   }
 }
