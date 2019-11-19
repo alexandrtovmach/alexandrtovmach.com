a7b5964a460d63c22b07d8f2ca8bba94fbd452be
alexandrtovmach <alexandrtovmach@gmail.com>
2019-11-19 13:33:18 +0200

    refactor: Formatted utils/task

--- utils/task.js
+++ utils/task.js
@@ -1,14 +1,14 @@
 const path = require("path");
 const fs = require("fs");
-const webp = require('webp-converter');
+const webp = require("webp-converter");
 const { generateResponsiveImages } = require("responsive-images-generator/lib/index");
 
 const IMAGES_DIR = path.join(`${__dirname}/images`);
 
 const configs = [
-  {width: '25%', rename: {suffix: '@1x'}},
-  {width: '50%', rename: {suffix: '@2x'}},
-  {width: '100%', rename: {suffix: '@3x'}}
+  { width: "25%", rename: { suffix: "@1x" } },
+  { width: "50%", rename: { suffix: "@2x" } },
+  { width: "100%", rename: { suffix: "@3x" } }
 ];
 
 Promise.all(
@@ -16,11 +16,12 @@ Promise.all(
     return new Promise(resolve => {
       const [name, ext] = file.split(".");
       if (ext !== "webp") {
-        webp.cwebp(`${IMAGES_DIR}/${file}`, `${IMAGES_DIR}/${name}.webp`, "-q 50", () => resolve(`${IMAGES_DIR}/${name}.webp`));
+        webp.cwebp(`${IMAGES_DIR}/${file}`, `${IMAGES_DIR}/${name}.webp`, "-q 50", () =>
+          resolve(`${IMAGES_DIR}/${name}.webp`)
+        );
       } else {
         resolve(`${IMAGES_DIR}/${file}`);
       }
-    })
+    });
   })
-)
-.then(images => generateResponsiveImages(images, configs));
+).then(images => generateResponsiveImages(images, configs));
