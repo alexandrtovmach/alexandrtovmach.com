6c9e9c29e9d1b1c3f85426254f3df3a3ea12e4c3
alexandrtovmach <alexandrtovmach@gmail.com>
2020-02-13 00:04:50 +0200

    feat: Completed all langs translations

--- src/utils/resume/resumeBuild.js
+++ src/utils/resume/resumeBuild.js
@@ -3,16 +3,12 @@ const path = require("path");
 const handlebars = require("handlebars");
 
 const enContent = require("../translations/en");
-// const ruContent = require("../translations/ru");
-// const uaContent = require("../translations/ua");
+const ruContent = require("../translations/ru");
+const uaContent = require("../translations/ua");
 
 const source = fs.readFileSync(`${path.resolve()}/src/utils/resume/template.hbs`, "utf8");
 
-[
-  enContent
-  // ruContent,
-  // uaContent
-].forEach(content => {
+[enContent, ruContent, uaContent].forEach(content => {
   const template = handlebars.compile(source, { strict: true });
   const result = template(content);
 
