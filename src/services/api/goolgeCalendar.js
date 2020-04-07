065f23bbec1c46f84d14baaaa5d817b815fbf40e
alexandrtovmach <alexandrtovmach@gmail.com>
2020-04-07 11:13:44 +0300

    feat: Updated calendar events fetch

--- src/services/api/goolgeCalendar.js
+++ src/services/api/goolgeCalendar.js
@@ -1,16 +1,18 @@
-const API_KEY = "AIzaSyBS4jzKakkbQqT1nLoWeAXMhhv0zddLLMU";
-const CALENDAR_ID = "25b7e2v5vatnp8e9a343iu0p0o@group.calendar.google.com";
+const API_KEY = "AIzaSyDK1TJCRNnfQQZ6d1q40DLbOmm7d44S2QI";
+const CALENDAR_ID = "alexandrtovmach%40gmail.com";
 
-export function pullEventsFromAPI(count) {
+export const pullEventsFromAPI = async count => {
   const minDate = new Date();
   minDate.setHours(0);
   minDate.setMinutes(0);
   minDate.setSeconds(0);
   minDate.setMilliseconds(0);
-  const url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events/?key=${API_KEY}&maxResults=${count}&timeMin=${minDate.toISOString()}`;
-  return fetch(url)
-    .then(data => data.json())
-    .catch(err => {
-      console.error(err);
-    });
-}
+  const maxDate = new Date();
+  maxDate.setDate(minDate.getDate() + Number(count || 1));
+  const url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events/?key=${API_KEY}&maxResults=${count}&timeMin=${minDate.toISOString()}&timeMax=${maxDate.toISOString()}`;
+  const eventsResponse = await fetch(url);
+  const recurringEventsResponse = await fetch(url + "&singleEvents=true");
+  const events = await eventsResponse.json();
+  const recurringEvents = await recurringEventsResponse.json();
+  return [...events.items, ...recurringEvents.items];
+};
