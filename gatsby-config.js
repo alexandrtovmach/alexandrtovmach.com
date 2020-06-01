2b1d3c852eac4b58e58cdd4b2aa377e03f3633cf
alexandrtovmach <alexandrtovmach@gmail.com>
2020-06-02 01:52:50 +0300

    feat: Added sass and a11y

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
     {
---
+++ src/containers/Layout/index.tsx
@@ -0,0 +1,31 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StaticQuery, graphql } from 'gatsby';
+
+import Header from '../../components/header';
+import styles from './layout.module.scss';
+
+const Layout = ({ children }) => (
+  <div className={styles.pageWrapper}>
+    <Header siteTitle={'Alexandr Tovmach website'} />
+    <main>{children}</main>
+    <footer>
+      © {new Date().getFullYear()}, Built with
+      {` `}
+      <a href="https://www.gatsbyjs.org">Gatsby</a>
+    </footer>
+  </div>
+);
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
