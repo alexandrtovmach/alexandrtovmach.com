ae7a99aa99af5e553f199b7e1f99c88c35ac183a
alexandrtovmach <alexandrtovmach@gmail.com>
2020-10-21 22:43:31 +0300

    feat: Updated content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,19 +31,107 @@ const IndexPage = () => (
         )}
       />
       <article>
-        <h1>Hello, I'm Alexandr Tovmach</h1>
+        <h1>Hi, I'm Alexandr Tovmach.</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut <a href="/">labore et dolore</a> magna
-          aliqua.
+          I founded several open source packages like:{' '}
+          <a
+            target="_blank"
+            title="react-microsoft-login"
+            rel="noopener noreferrer"
+            href="https://www.npmjs.com/package/react-microsoft-login"
+          >
+            react-microsoft-login
+          </a>
+          ,{' '}
+          <a
+            target="_blank"
+            title="react-figma-plugin-ds"
+            rel="noopener noreferrer"
+            href="https://www.npmjs.com/package/react-figma-plugin-ds"
+          >
+            react-figma-plugin-ds
+          </a>{' '}
+          , and actively contributing to{' '}
+          <a
+            target="_blank"
+            title="Node.js"
+            rel="noopener noreferrer"
+            href="https://github.com/nodejs/"
+          >
+            Node.js
+          </a>
+          ,{' '}
+          <a
+            target="_blank"
+            title="SemVer"
+            rel="noopener noreferrer"
+            href="https://github.com/semver/"
+          >
+            SemVer
+          </a>
+          ,{' '}
+          <a
+            target="_blank"
+            title="Gatsby"
+            rel="noopener noreferrer"
+            href="https://github.com/gatsbyjs/"
+          >
+            Gatsby
+          </a>
+          ,{' '}
+          <a
+            target="_blank"
+            title="Friends of Figma"
+            rel="noopener noreferrer"
+            href="https://friends.figma.com/plugins/"
+          >
+            Friends of Figma
+          </a>{' '}
+          projects.
         </p>
         <p>
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat.
+          I write and translate articles on{' '}
+          <a
+            target="_blank"
+            title="Medium"
+            rel="noopener noreferrer"
+            href="https://medium.com/@alexandrtovmach"
+          >
+            Medium
+          </a>
+          ,{' '}
+          <a
+            target="_blank"
+            title="Habr"
+            rel="noopener noreferrer"
+            href="https://habr.com/ru/users/alexandrtovmach/posts/"
+          >
+            Habr
+          </a>
+          ,{' '}
+          <a
+            target="_blank"
+            title="DOU"
+            rel="noopener noreferrer"
+            href="https://dou.ua/users/aleksandr-tovmach/articles"
+          >
+            DOU
+          </a>
+          , and also speak at meetups and{' '}
+          <a
+            target="_blank"
+            title="JSFest 2019: JAMStack"
+            rel="noopener noreferrer"
+            href="https://www.youtube.com/watch?v=wCNSK4iFCuE"
+          >
+            conferences
+          </a>
+          .
         </p>
         <p>
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-          dolore eu fugiat nulla pariatur.
+          I'm full stack engineer & UI/UX designer, and my expertise is JS
+          ecosystem, especially React and Node.js. But my vision and real
+          experience significantly wider.
         </p>
       </article>
     </section>
