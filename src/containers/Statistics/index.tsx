04582afac527c8744b197b9d2b18b15788348758
Alexandr Tovmach <alexandrtovmach@gmail.com>
2021-12-10 12:08:27 +0200

    feat: Styled book shelf

--- src/containers/Statistics/index.tsx
+++ src/containers/Statistics/index.tsx
@@ -10,21 +10,21 @@ const Statistics: React.FunctionComponent<StatisticsProps> = ({ books }) => {
   return (
     <main className={styles.stats}>
       <section>
-        <h3 className={styles.sectionTitle}>Books list</h3>
-        <ul>
+        <h3 className="section-title">Books list</h3>
+        <ul className={styles.bookShelf}>
           {books.map(({ firstImage, title }) => (
-            <div>
-              <img src={firstImage} alt={title} />
+            <li>
+              {/* <img src={firstImage} alt={title} /> */}
               <h3>{title}</h3>
-            </div>
+            </li>
           ))}
         </ul>
       </section>
       <section>
-        <h3 className={styles.sectionTitle}>Visited concerts list</h3>
+        <h3 className="section-title">Visited concerts list</h3>
       </section>
       <section>
-        <h3 className={styles.sectionTitle}>Movies list</h3>
+        <h3 className="section-title">Movies list</h3>
       </section>
     </main>
   );
