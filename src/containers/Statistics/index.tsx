e7171ad670fec274e136c6df2b4d7add6f50ad4e
Alexandr Tovmach <alexandrtovmach@gmail.com>
2021-12-16 02:19:10 +0200

    feat: Added posters

--- src/containers/Statistics/index.tsx
+++ src/containers/Statistics/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import sortBy from 'lodash/sortBy';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import * as styles from './stats.module.scss';
 
@@ -30,8 +31,56 @@ const Statistics: React.FunctionComponent<StatisticsProps> = ({ books }) => {
           />
         </ul>
       </section>
-      <section>
-        <h3 className="section-title">Visited concerts list</h3>
+      <section className={styles.posters}>
+        <StaticImage
+          alt="Morphine Suffering: MADISAN 2012"
+          className={styles.poster}
+          src="../../assets/images/posters/morphine_2012.jpeg"
+          placeholder="blurred"
+          layout="fullWidth"
+        />
+        <StaticImage
+          alt="O.Torvald: MADISAN 2012"
+          className={styles.poster}
+          src="../../assets/images/posters/otorvald_2012.jpeg"
+          placeholder="blurred"
+          layout="fullWidth"
+        />
+        <StaticImage
+          alt="ПОРНОФИЛЬМЫ: Picasso 2018"
+          className={styles.poster}
+          src="../../assets/images/posters/pornofilmi_2018.jpeg"
+          placeholder="blurred"
+          layout="fullWidth"
+        />
+        <StaticImage
+          alt="Enter Shikari: !FEST Republic 2019"
+          className={styles.poster}
+          src="../../assets/images/posters/entershikari_2019.jpeg"
+          placeholder="blurred"
+          layout="fullWidth"
+        />
+        <StaticImage
+          alt="Друга Ріка: Malevich 2019"
+          className={styles.poster}
+          src="../../assets/images/posters/drugarika_2019.jpeg"
+          placeholder="blurred"
+          layout="fullWidth"
+        />
+        <StaticImage
+          alt="Bullet For My Valentine: Stereo Plaza 2019"
+          className={styles.poster}
+          src="../../assets/images/posters/bfmv_2019.jpeg"
+          placeholder="blurred"
+          layout="fullWidth"
+        />
+        <StaticImage
+          alt="RadioTapok: Malevich 2020"
+          className={styles.poster}
+          src="../../assets/images/posters/radiotapok_2020.jpeg"
+          placeholder="blurred"
+          layout="fullWidth"
+        />
       </section>
       <section>
         <h3 className="section-title">Movies list</h3>
--- src/files.d.ts
+++ src/files.d.ts
@@ -2,4 +2,5 @@ declare module '*.scss';
 declare module '*.svg';
 declare module '*.webp';
 declare module '*.jpg';
+declare module '*.jpeg';
 declare module '*.png';
