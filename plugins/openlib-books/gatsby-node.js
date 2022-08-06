aeabe90a9895d498bdbb9b46f762747df83bb8f5
alexandrtovmach <alexandrtovmach@gmail.com>
2022-08-06 22:00:16 +0200

    feat: Updated dependencies

--- plugins/openlib-books/gatsby-node.js
+++ plugins/openlib-books/gatsby-node.js
@@ -1,13 +1,13 @@
 const fetch = require(`node-fetch`);
-const path = require(`path`);
+const { resolve } = require(`path`);
 const { getAverageColor } = require(`fast-average-color-node`);
-const { meanBy, isEqual, sortBy, get, filter } = require('lodash');
+const { meanBy, isEqual, sortBy } = require(`lodash`);
 
 const LOG_PREFIX = '[openlib-books]: ';
 const SOURCE_CACHE_KEY = 'openlib-books-source';
 const DATA_CACHE_KEY = 'openlib-books-data';
 const OPEN_LIB_URL = 'https://openlibrary.org';
-const CYRILLIC_PATTERN = /^[\u0400-\u04FF -,.!]+$/;
+// const CYRILLIC_PATTERN = /^[\u0400-\u04FF -,.!]+$/;
 
 const getColorFromImageSrc = async (imgSrc, reporter) => {
   try {
@@ -25,25 +25,25 @@ const getColorFromImageSrc = async (imgSrc, reporter) => {
   }
 };
 
-const getCoverImage = (work, editions) => {
-  const workLang =
-    get(work, 'languages[0].key') || CYRILLIC_PATTERN.test(work.title)
-      ? '/languages/rus'
-      : '/languages/eng';
-  const filteredEditions = editions.entries
-    .filter(
-      ({ covers, publish_date }) =>
-        publish_date &&
-        covers &&
-        covers.length
-    )
-    .sort((a, b) => new Date(b.publish_date) - new Date(a.publish_date));
+// const getCoverImage = (work, editions) => {
+//   const workLang =
+//     get(work, 'languages[0].key') || CYRILLIC_PATTERN.test(work.title)
+//       ? '/languages/rus'
+//       : '/languages/eng';
+//   const filteredEditions = editions.entries
+//     .filter(
+//       ({ covers, publish_date }) =>
+//         publish_date &&
+//         covers &&
+//         covers.length
+//     )
+//     .sort((a, b) => new Date(b.publish_date) - new Date(a.publish_date));
     
-  const candidate = filteredEditions.filter((edition) => get(edition, 'languages[0].key') === workLang)[0] || filteredEditions[0];
-  const coverId = filteredEditions.length ? candidate.covers[0] : (work.covers || [])[0];
+//   const candidate = filteredEditions.filter((edition) => get(edition, 'languages[0].key') === workLang)[0] || filteredEditions[0];
+//   const coverId = filteredEditions.length ? candidate.covers[0] : (work.covers || [])[0];
 
-  console.log(`${work.title}[${Number(filteredEditions.length)}]: https://covers.openlibrary.org/b/id/${coverId}-L.jpg`);
-};
+//   console.log(`${work.title}[${Number(filteredEditions.length)}]: https://covers.openlibrary.org/b/id/${coverId}-L.jpg`);
+// };
 
 exports.sourceNodes = async (
   { actions, createNodeId, createContentDigest, reporter, cache },
@@ -55,7 +55,7 @@ exports.sourceNodes = async (
   }
   reporter.info(`${LOG_PREFIX}Start`);
 
-  const sourcesList = require(path.resolve(pluginOptions.listSrc));
+  const sourcesList = require(resolve(pluginOptions.listSrc));
 
   if (!Array.isArray(sourcesList) || !sourcesList.length) {
     reporter.error(`${LOG_PREFIX}"listSrc" empty or incorrect.`);
@@ -106,6 +106,10 @@ exports.sourceNodes = async (
             `${OPEN_LIB_URL}${workData.authors[0].author.key}.json`
           );
           const authorData = await authorDataRes.json();
+          if (!workData.authors) {
+            reporter.error(`${LOG_PREFIX}Work ${id} not found`);
+            return;
+          }
           const editionsDataRes = await fetch(
             `${OPEN_LIB_URL}/works/${id}/editions.json`
           );
@@ -168,4 +172,4 @@ exports.sourceNodes = async (
     reporter.info(`${LOG_PREFIX}Saved to cache`);
   }
   reporter.info(`${LOG_PREFIX}End`);
-};
+}
