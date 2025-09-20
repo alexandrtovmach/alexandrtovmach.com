34d557c0c4a820100b2c8b14aa3129ae5e9f98b1
alexandrtovmach <alexandrtovmach@gmail.com>
2025-09-20 22:10:46 +0200

    fix: Static skills

--- src/components/SkillList.tsx
+++ src/components/SkillList.tsx
@@ -1,55 +1,15 @@
-import { useEffect, useState } from "react";
-
 export interface SkillItem {
   name: string;
   isText?: boolean;
 }
 
 export const SkillListItem: React.FC<SkillItem> = ({ name, isText }) => {
-  const [isHovered, setIsHovered] = useState(false);
-  const className =
-    "inline font-bold text-xs px-2 rounded-full transition-all duration-[200ms] text-sm border border-gray-300 cursor-default opacity-70";
-
-  const hoverClass = "bg-black text-white border-white";
-
-  // Update hover state if URL hash matches this skill name
-  useEffect(() => {
-    const checkHash = () => {
-      console.log("Checking hash for", name);
-      const hash = decodeURIComponent(window.location.hash.slice(1));
-      console.log(hash === name);
-      setIsHovered(hash === name);
-    };
-
-    // Check initially
-    checkHash();
-
-    // Listen to hash changes
-    window.addEventListener("hashchange", checkHash);
-    return () => window.removeEventListener("hashchange", checkHash);
-  }, [name]);
-
-  // On mouse over, update URL hash
-  const handleMouseOver = () => {
-    window.location.hash = encodeURIComponent(name);
-  };
-
+  const className = "inline font-bold text-xs px-2 rounded-full transition-all duration-[200ms] text-sm border border-gray-300 cursor-default opacity-70 hover:bg-black hover:text-white hover:border-white";
   if (isText) {
-    return (
-      <span
-        className={`${className} ${isHovered ? hoverClass : ""}`}
-        onMouseOver={handleMouseOver}
-      >
-        {name}
-      </span>
-    );
+    return <span className={className}>{name}</span>;
   }
-
   return (
-    <li
-      className={`${className} ${isHovered ? hoverClass : ""}`}
-      onMouseOver={handleMouseOver}
-    >
+    <li className={className}>
       {name}
     </li>
   );
